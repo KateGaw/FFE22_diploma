eb2bd9029dc31e7fb1fd10898a16a2cc81949813
kate_gaw <ekaterinagaw@gmail.com>
2020-08-10 17:48:11 +0300

    Create Preloader.js

---
+++ src/components/elements/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Preloader() {
+  return (
+    <div className="preloader">
+      <img src='assets/preloader.gif' alt='preloader' />
+    </div>
+  );
+}
+
+export default Preloader;
